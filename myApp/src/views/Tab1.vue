b3de7351584c575b345311f396b798f0295241ad
Chavengsak Pattanaponpaiboon <changesak@gmail.com>
2020-12-18 10:43:53 +0700

    commit first version

---
+++ myApp/src/views/Tab1.vue
@@ -0,0 +1,34 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-card>
+    <ion-card-header>
+      <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+      <ion-card-title>Card Title</ion-card-title>
+    </ion-card-header>
+    <img src="https://source.unsplash.com/XCkc8-_s7FA/400x300">
+    
+    <ion-card-content>
+      Keep close to Nature's heart... and break clear away, once in awhile,
+      and climb a mountain or spend a week in the woods. Wash your spirit clean.
+    </ion-card-content>
+  </ion-card>
+   <ion-input placeholder="Enter Input"></ion-input>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent,IonCard, IonCardContent, IonCardSubtitle, IonCardTitle,IonInput } from '@ionic/vue';
+
+
+export default  {
+  name: 'Tab1',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage,IonCard, IonCardContent, IonCardSubtitle, IonCardTitle,IonInput }
+}
+</script>
