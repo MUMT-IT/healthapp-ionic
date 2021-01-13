bccafcbfbb88380ee4ce2c423fbbb412ad236672
Chavengsak Pattanaponpaiboon <changesak@gmail.com>
2021-01-13 09:57:44 +0700

    Change ProfileEditSave Name,Gender,Birthdate

--- myApp/src/Profile/ProfileEditSave.vue
+++ myApp/src/Profile/ProfileEditSave.vue
@@ -21,47 +21,6 @@
                 <ion-datetime value="1990-02-19" placeholder="Select Date" picker-format="DD/MM/YYYY" slot="end"></ion-datetime>
             </ion-item>
 
-             <ion-item>
-                <ion-label>
-                    Weight:
-                </ion-label>
-                  <ion-input placeholder="Enter Input" slot="end"></ion-input>
-            </ion-item>
-             <ion-item>
-                <ion-label>
-                    Height:
-                </ion-label>
-                  <ion-input placeholder="Enter Input" slot="end"></ion-input>
-            </ion-item>
-
-            <ion-item>
-                <ion-label>Exercise</ion-label>
-                    <ion-select :interface-options="customPopoverOptions" interface="popover" placeholder="Select One">
-                        <ion-select-option value="brown">No</ion-select-option>
-                        <ion-select-option value="blonde">1 day/week</ion-select-option>
-                        <ion-select-option value="blonde">2-3 day/week</ion-select-option>
-                        <ion-select-option value="blonde">4-7 day/week</ion-select-option>
-                </ion-select>
-            </ion-item>
-
-            <ion-item>
-                <ion-label>Congenital disease</ion-label>
-                <ion-list>
-                    <ion-item>
-                        <ion-label>diabetes</ion-label>
-                        <ion-checkbox color="primary"></ion-checkbox>
-                    </ion-item>
-                    <ion-item>
-                        <ion-label>Obesity</ion-label>
-                        <ion-checkbox color="primary"></ion-checkbox>
-                    </ion-item>
-                    <ion-item>
-                        <ion-label>Hypertension</ion-label>
-                        <ion-checkbox color="primary"></ion-checkbox>
-                    </ion-item>
-                </ion-list>
-                
-            </ion-item>
         </ion-list>
      
         <ion-button type="submit" expand="block">Save</ion-button>
@@ -69,7 +28,7 @@
 </template>
 <script>
 
-import {IonList,IonItem,IonLabel,IonInput,IonButton,IonDatetime,IonSelect,IonSelectOption,IonCheckbox} from '@ionic/vue';
+import {IonList,IonItem,IonLabel,IonInput,IonButton,IonDatetime} from '@ionic/vue';
 export default {
      components:{
         IonList,
@@ -77,10 +36,7 @@ export default {
         IonLabel,
         IonInput,
         IonDatetime,
-        IonSelect,
-        IonSelectOption,
-        IonButton,
-        IonCheckbox
+        IonButton
     },
     data(){
         return{
