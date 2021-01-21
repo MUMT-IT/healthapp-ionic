090be5838b6f760024c8d427c3ede685938b84e8
Chavengsak Pattanaponpaiboon <changesak@gmail.com>
2021-01-21 15:44:08 +0700

    Create Code HealthAddPage

--- myApp/src/pages/HealthAddPage.vue
+++ myApp/src/pages/HealthAddPage.vue
@@ -1,15 +1,118 @@
 <template>
     <base-layout>
     <health-menu></health-menu>
-        <h2>Health Add Page</h2>
+        <form class="ion-padding" @submit.prevent="submitForm">
+            <ion-list>
+                <ion-item>
+                    <ion-label>Date</ion-label>
+                    <ion-datetime value="1990-02-19" placeholder="Select Date" picker-format="DD/MM/YYYY" slot="end" v-model="dateresult"></ion-datetime>
+                </ion-item>
+
+                <ion-item>
+                    <ion-label>Weight</ion-label>
+                    <ion-input slot="end"  type="text" v-model="weight"></ion-input>
+                </ion-item>
+
+                <ion-item>
+                    <ion-label>Height</ion-label>
+                    <ion-input slot="end" type="text" v-model="height"></ion-input>
+                </ion-item>
+
+                <ion-item>
+                    <ion-label>Heartrate</ion-label>
+                    <ion-input slot="end" type="text"></ion-input>
+                </ion-item>
+
+                <ion-item>
+                    <ion-label>Systolic</ion-label>
+                    <ion-input slot="end" type="number"></ion-input>
+                    <ion-label slot="end">/</ion-label>
+                    <ion-input slot="end" type="number"></ion-input>
+                </ion-item>
+            </ion-list>
+            <ion-button type="submit" expand="block">Save</ion-button>
+        </form>
+        <p></p>
+        <h2>Result</h2>
+        <ion-list>
+            <ion-item>
+                <ion-select interface="popover" v-model="Test">
+                    <ion-select-option value="GLU">GLU</ion-select-option>
+                    <ion-select-option value="CHO">Chol</ion-select-option>
+                    <ion-select-option value="TG">TG</ion-select-option>
+                    <ion-select-option value="HDL">HDL</ion-select-option>
+                    <ion-select-option value="LDL">LDL</ion-select-option>
+                </ion-select>
+                <ion-input type="number" text-right v-model="Result"></ion-input>
+                <ion-buttons slot="end">
+                    <ion-button v-on:click="AddTest"><ion-icon :icon="saveOutline" /></ion-button>
+                </ion-buttons>
+            </ion-item>
+        </ion-list>
+
+        <ion-list>
+            <ion-item v-for="(Titem) in TestData" :key="Titem.id">
+                <label slot="start">{{ Titem.Test }}</label>
+                <label>{{ Titem.Result }}</label>
+                <ion-buttons slot="end">
+                    <ion-button v-on:click="deleEvent(Titem.id)"><ion-icon :icon="trashOutline"/></ion-button>
+                </ion-buttons>
+            </ion-item>
+        </ion-list>
     </base-layout>
 </template>
 <script>
-import HealthMenu from '../Health/HealthMenu.vue'
+import { saveOutline,trashOutline } from 'ionicons/icons';
+import HealthMenu from '../Health/HealthMenu.vue';
+import {IonList,IonItem,IonDatetime,IonInput,IonButton,IonSelect,IonSelectOption,IonLabel,IonButtons} from '@ionic/vue';
 export default {
   components: { 
-      HealthMenu
+      HealthMenu,
+      IonList,
+      IonItem,
+      IonDatetime,
+      IonInput,
+      IonButton,
+      IonSelect,
+      IonSelectOption,
+      IonLabel,
+      IonButtons
    },
+   data(){
+       return{
+           count: 0,
+           Test: "",
+           Result: "",
+           weight: "",
+           height: "",
+           TestData:[]
+       }
+   },
+    methods:{
+        submitForm(){
+            //this.$router.push({path:'/profile', query: { fullname: this.Test,dis: 1224 }})
+        },
+        AddTest(){
+             const newTestData = {
+                 id: this.count += 1,
+                 Test: this.Test,
+                 Result: this.Result,
+                 DateReult: this.dateresult
+             };
+             this.TestData.unshift(newTestData);
+             this.Test = "",
+             this.Result = ""
+        },
+        deleEvent(id){
+        this.TestData = this.TestData.filter((e)=>e.id !== id )
+        }
+    },
+    setup(){
+        return{
+            saveOutline,
+            trashOutline
+        }
+    },
     
 }
 </script>
